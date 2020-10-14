b8217567ce4d4cd5521bfc15cc1a30ff0f8c0f45
Justin Artis <jusart1s8@gmail.com>
2020-10-14 17:54:35 -0400

    added password
    will add encyption and clean up later

--- server.py
+++ server.py
@@ -12,22 +12,68 @@ dynamodb = boto3.resource('dynamodb')
 def hello_world():
     return 'Hello, Emma!'
 
+
+def convert_byte_data(user_data):
+    return dict([(str(k), str(v)) for k, v in user_data.items()])
+
+
 @app.route('/getUser',methods=['GET'])
 def get_user():
-    dynamoDB = boto3.resource('dynamodb')
-    user_table = dynamoDB.Table('user_data')
+    # dynamoDB = boto3.resource('dynamodb')
+    user_table = dynamodb.Table('user_data')
     print(user_table.creation_date_time)
     print(user_table.key_schema)
     # print(type(request.form['id']))
     user_data  = user_table.get_item(Key={'username': request.form['username']})
     print(type(user_data))
     user_data = user_data[u'Item']
-    user_items = dict([(str(k), str(v)) for k, v in user_data.items()])
+    user_items = convert_byte_data(user_data)
     print(user_items)
     #jsonify(user_data)
     return jsonify(user_items)
 
-@app.route('/test',methods= ['GET'])
+@app.route('/updateEmail',methods=['POST'])
+def update_email():
+    print(request.form)
+    user_table = dynamodb.Table('user_data')
+    print(request.form['new_email'])
+    new_email= request.form['new_email']
+    response = user_table.update_item(Key={
+            'username': request.form['username']
+        },
+        UpdateExpression="set email=:e",
+        ExpressionAttributeValues={
+            ':e': new_email
+        },
+        ReturnValues="UPDATED_NEW")
+    return "200"
+
+@app.route('/updatePassword',methods=['POST'])
+def update_password():
+    # print(request.form)
+    user_table = dynamodb.Table('user_data')
+    user_data = user_table.get_item(Key={'username': request.form['username']})
+    user_data = user_data[u'Item']
+    user_items = convert_byte_data(user_data)
+    username = request.form['username']
+    old_password = request.form['current_password']
+    new_password = request.form['new_password']
+    print(old_password)
+    if old_password != user_items['password']:
+        return "500"
+    print(request.form['new_password'])
+    response = user_table.update_item(Key={
+        'username': username
+    },
+        UpdateExpression="set password=:p",
+        ExpressionAttributeValues={
+            ':p': new_password
+        },
+        ReturnValues="UPDATED_NEW")
+    # response
+    return response
+
+@app.route('/test',methods=['GET'])
 def test():
     return '''<h1>Cultivida</h1>
 <p>Emma and Ray is going to the Seven Walls</p>'''
